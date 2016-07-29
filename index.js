f257fea8cfea8055ec6198c911c66fe945e64ec9
AlexWang <1669499355@qq.com>
2016-07-29 19:34:31 +0800

    local_dev_commit

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-module.exports = function init(params, options) {
-  console.log(params, options);
-  console.dir(this);
+/*eslint-disable*/
+var form = require('inquirer').createPromptModule();
+var fs = require('promisify-fs');
+
+//global context
+var context = null;
+
+module.exports = exports = function init(params, options) {
+  //init plugin
+  context = this;
+
+  //invoke the initor specified by user.
+  if (params && params.length) {
+    var initor = params[0];
+    return context.call('init-' + initor);
+  }
+
+  //selects
+  return form({
+      message: 'Which project are you going to start?',
+      type: 'list',
+      name: 'project_type',
+      choices: function () {
+        return fs
+          .getModulePackInfo(module)
+          .get('wbp_init_projects')
+          .then(function (projects) {
+            return projects && projects.map(function (project) {
+              return project.name
+            })
+          })
+      }
+    })
+    .then(function (answers) {
+      context.call('init-' + answers.project_type.toLowerCase());
+    })
 };
 
